5e78f3d031e2a97cfc3b4533069613c4be55af71
gioalexrr90 <gioaxel90@gmail.com>
2022-11-26 06:17:03 -0600

    Se elimina console.log inecesario

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -39,7 +39,7 @@ const validarJWT = async( req, res = response, next ) => {
         next();
         
     } catch (error) {
-        console.log(error);
+        //console.log(error);
         res.status(401).json({
             msg: 'Token no valido'
         })
