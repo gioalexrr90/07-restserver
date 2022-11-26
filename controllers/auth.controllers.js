95a448cfa78dc961984d4828e5305ec386a0363c
gioalexrr90 <gioaxel90@gmail.com>
2022-11-26 01:12:52 -0600

    Udemy - S10 C141

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,10 +1,48 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const User = require('../models/user');
 
-const login = (req = request, res = response) => {
+const login = async(req = request, res = response) => {
 
-    res.json({
-        msg: 'Login ok'
-    })
+    const { email, password } = req.body;
+
+    try {
+
+        // Verificar si el email existe
+        const user = await User.findOne({ email });
+        if( !user ){
+            return res.status(400).json({
+                msg: 'Usuario o contraseña no son correctos - correo'
+            })
+        }
+
+        // Validar si el usuario esta activo
+        if( !user.status ){
+            return res.status(400).json({
+                msg: 'Usuario o contraseña no son correctos - status: false'
+            })
+        }
+
+        // Verificar contraseña
+        const validarPassword = bcryptjs.compareSync(password, user.password);
+        if( !validarPassword ){
+            return res.status(400).json({
+                msg: 'Usuario o contraseña no son correctos - password'
+            })
+        }
+
+        // General JWT
+
+        res.json({
+            msg: 'Login ok'
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 module.exports = {
