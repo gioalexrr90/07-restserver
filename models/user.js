2f97a9e6a891a12fd2b97d34bc2ebad74df11d2f
gioalexrr90 <gioaxel90@gmail.com>
2022-11-26 03:51:01 -0600

    Se cambia el _id por uid en la respuesta de Postman

--- models/user.js
+++ models/user.js
@@ -38,9 +38,11 @@ const userSchema = Schema({
     },
 });
 
-//Esta funcioncion permite liminar del resultado el __v y password como respuesta en el res, enviando solo "usuario" que son los datos restantes 
+//Esta funcioncion permite eliminar del resultado el __v y password como respuesta en el res, enviando solo "usuario" que son los datos restantes 
 userSchema.methods.toJSON = function() {
-    const { __v, password, ...usuario } = this.toObject();
+    const { __v, password, _id, ...usuario } = this.toObject();
+    // Se cambia el dato de _id a uid en los resultados de Postman
+    usuario.uid = _id;
     return usuario;
 }
 
